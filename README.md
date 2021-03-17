8f4967bf95fa4277396faccc9887b6429f1b9e16
Dylan Madisetti <dylan.madisetti@gmail.com>
2021-03-17 22:28:58 +0000

    Add progress checking to project

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Painful Haskell <img align="right" src="https://projecteuler.net/profile/PainfulHaskell.png">
 
-# NOTE:
+### NOTE:
 *Please only use this repository as a reference, once you have solved the problems yourself. No solution problem over problem 100 is provided.*
+
+<img align="center" src="https://raw.githubusercontent.com/dmadisetti/painfulhaskell/gh-pages/progress.svg">
---
+++ scripts/gen_svg.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys
+import re
+import json
+from math import log
+import requests
+
+pattern = r"^.*_(\d+)_.*(\d+.\d+)s"
+
+tests = []
+times = []
+
+for line in sys.stdin:
+    match = re.match(pattern, line)
+    test, time = match.groups()
+    tests.append(int(test))
+    times.append(log(max(float(time), 0.1)))
+
+fastest = min(times)
+times = list(map(lambda t: (t - fastest) + 1, times))
+slowest = max(times) / 4.0
+times = list(map(lambda t: 5 - max(1, int(t / slowest)), times))
+
+results = dict(zip(tests, times))
+
+data = {"data": [], "themes": ["#AAA", "#966fd6"]}
+
+# create 10x10 grids with gap bricks
+for c in range(8):
+    ci = (c // 4) * 11
+    cj = (c % 4) * 11
+    for i in range(1, 11):
+        for j in range(1, 11):
+            index = c * 100 + i * 10 + j
+            if index in results:
+                data["data"].append([cj + j, ci + i, results[index], 1])
+            else:
+                data["data"].append([cj + j, ci + i, 1])
+
+r = requests.post(
+    "https://endpointservice.web.app/notebooks/@dmadisetti/contribution-tracker/deployments/contribution",
+    json=data)
+print(r.content.decode('UTF-8'))
