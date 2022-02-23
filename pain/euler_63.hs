62f7896ce4244d2ae366c96d5b5fbb4fdfd0471c
dylan <dylan@madisetti.me>
2022-02-23 23:59:34 +0000

    63, 79, some easy ones

---
+++ pain/euler_63.hs
@@ -0,0 +1,17 @@
+{- Project Euler
+Problem 63
+==========
+
+
+   The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the
+   9-digit number, 134217728=8^9, is a ninth power.
+
+   How many n-digit positive integers exist which are also an nth power?
+
+
+   Answer: f457c545a9ded88f18ecee47145a72c0
+-}
+
+
+main :: IO ()
+main = print $ sum $ [floor $ (log 10)/(log (10 / x)) | x <- [1..9] :: [Double]]
---
+++ pain/euler_79.hs
@@ -0,0 +1,71 @@
+{- Project Euler
+Problem 79
+==========
+
+
+   A common security method used for online banking is to ask the user for
+   three random characters from a passcode. For example, if the passcode was
+   531278, they may ask for the 2nd, 3rd, and 5th characters; the expected
+   reply would be: 317.
+
+   The text file, [1]keylog.txt, contains fifty successful login attempts.
+
+   Given that the three characters are always asked for in order, analyse the
+   file so as to determine the shortest possible secret passcode of unknown
+   length.
+
+
+   Visible links
+   1. keylog.txt
+   Answer: 3ccc6e16d99b21d42948f6d49b90fa30
+-}
+
+import           Data.HashMap (Map, empty, keys, elems, insert, insertWith, findWithDefault, toList, lookup)
+import           System.IO
+import           Data.Char (isSpace)
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
+
+type Dag   = Map Char [Char]
+type Cache = Map Char [Char]
+
+search :: Dag -> [Char]
+search d = foldl greedy "" paths
+   where
+      paths = elems $ foldl traverse (empty :: Cache) (toList d)
+      -- traverse - will blow up if not acyclic
+      traverse :: Cache -> (Char, String) -> Cache
+      traverse m (k, v) = cacheOrSearch $ Data.HashMap.lookup k m
+         where
+            cacheOrSearch Nothing = insert k (k:new) m'
+            cacheOrSearch _ = m
+            new = foldl greedy "" (elems m')
+            m' = foldl traverse m (zip v (map byKey v))
+      greedy a b
+         | (length a) > (length b) = a
+         | otherwise               = b
+      byKey = (flip (findWithDefault "") d)
+
+process :: [String] -> String
+process entries = search dag
+   where
+      process' :: Dag -> [String] -> Dag
+      process' r (e:es) = process' (chain r e) es
+      process' r [] = r
+      chain :: Dag -> String -> Dag
+      chain r [a, b, c] = r''
+         where
+            r' = insertWith (++) a [b] r
+            r'' = insertWith (++) b [c] r'
+      chain r _ = r
+      root = empty :: Dag
+      dag = process' root entries
+
+main :: IO ()
+main = do
+  handle <- openFile "external/euler/files/keylog.txt" ReadMode
+  contents <- hGetContents handle
+  putStrLn $ process $ map trim $ lines contents
+  hClose handle
